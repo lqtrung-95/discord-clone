c5814a7caa8bb98647d0f07590fa712c609187c9
Trung Le <letrung95@gmail.com>
2021-07-28 12:50:54 +0700

    create invite page

--- client/src/pages/Invite.js
+++ client/src/pages/Invite.js
@@ -7,6 +7,31 @@ import { useQueryClient } from "react-query";
 
 export default function Invite() {
   const [errors, setErrors] = useState(null);
+  const { link } = useParams();
+
+  const cache = useQueryClient();
+  const history = useHistory();
+
+  useEffect(() => {
+    async function handleJoin() {
+      try {
+        const { data } = await joinGuild({ link });
+        if (data) {
+          cache.invalidateQueries(gKey);
+          history.push(`/channels/${data.id}/${data.default_channel_id}`);
+        }
+      } catch (err) {
+        const status = err?.response?.status;
+        if (status === 400 || status === 404) {
+          setErrors({ link: err?.response?.data?.message });
+        } else {
+          setErrors("An error occured. Please try again later.");
+        }
+      }
+    }
+
+    handleJoin();
+  }, [link, history, cache]);
 
   return (
     <Flex minHeight="100vh" align="center" justify="center" h="full">
